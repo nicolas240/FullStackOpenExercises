05924d9152f4c2e0d8aa88b339d7065a8586b328
nicolas240 <nicolasmadero24@gmail.com>
2024-05-03 11:49:35 -0500

    1.12: anecdotes, pass 1

--- part1/1.6-1.14/src/App.jsx
+++ part1/1.6-1.14/src/App.jsx
@@ -1,148 +1,37 @@
 import { useState } from 'react'
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
-  
-  const sets ={
-    'good': setGood,
-    'neutral': setNeutral,
-    'bad': setBad
-  }
-
-  const states=[
-    {text:'good', value: good},
-    {text: 'neutral', value: neutral},
-    {text: 'bad', value: bad},
-    {text: 'all', value: all},
-    {text: 'average', value: average},
-    {text: 'positive', value: positive}
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
   ]
 
-  const handleClick =(type)=>{
-    let allSum =all+1
-    if(type==='good'){
-      return ()=>{
-        let count =good+1
-        sets[type](count)
-        setAll(allSum)
-        setPositive( count*100/allSum +'%')
-        setAverage( (count-bad)/allSum )
-      }
-    }else if (type==='neutral'){
-      return ()=>{
-        let count = neutral+1
-        sets[type](count)
-        setAll(allSum)
-        setPositive(good*100/allSum+'%')
-        setAverage( (good-bad)/allSum )
-      }
-    }else{
-      return ()=>{
-        let count =bad+1
-        sets[type](count)
-        setAll(allSum)
-        setPositive(good*100/allSum+'%')
-        setAverage( (good-count)/allSum )
-      }
-    }  
+  const [selected, setSelected] = useState(0)
+
+  const handleClick =()=>{
+    console.log(selected)
+    return ()=>{setSelected(Math.floor(Math.random()*8))
+    }
   }
-
   return (
-    <>
-      <div>
-        <FeedbackHeader>
-        </FeedbackHeader>
-        <Button handle={handleClick('good')} text={'good'}></Button>
-        <Button handle={handleClick('neutral')} text={'neutral'}></Button>
-        <Button handle={handleClick('bad')} text={'bad'}></Button>           
-      </div>
-      <div>
-        <Statistics data={states}>
-        </Statistics>
-      </div>
-    </>
-  )
-}
-
-//--FeedBack--
-const FeedbackHeader = ()=>{
-  return (
-    <>
-      <h1>Give us feedback</h1>
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button handle={handleClick()} text={'next anecdote'}></Button>
+    </div>
   )
 }
 
 const Button = (props)=>{
   return (
     <button onClick={props.handle}>
-      {props.text}
+      props.text
     </button>
-  )
-}
-
-//--Stastistics
-const StaticsHeader=()=>{
-  return (
-    <>
-      <h1>Statistics</h1>
-    </>
-  )
-}
-
-const Statistics = (props)=>{
-  let all
-  props.data.forEach(dato => {
-    if (dato.text==='all'){
-      all=dato.value
-    }
-  });
-  if (all<1){
-    return (
-      <>
-        <StaticsHeader>
-        </StaticsHeader>
-        <p>No feedback given</p>
-      </>
-    )
-  }else{
-    return (
-      <>
-        <StaticsHeader>
-        </StaticsHeader>
-        <TableStatistics data ={props.data}>
-        </TableStatistics>
-      </>
-    )
-  }
-}
-
-const TableStatistics = (props)=>{
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>Text</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.data.map((dato, index) => (
-            <tr key={index}>
-              <td>{dato.text}</td>
-              <td>{dato.value}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
   )
 }
 

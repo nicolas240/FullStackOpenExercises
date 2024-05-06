3d0e593fd156d5ebedc3dbd4a0f0320a40f57cca
nicolas240 <nicolasmadero24@gmail.com>
2024-05-06 16:05:17 -0500

    2.9: Phonebook, step 4

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -3,10 +3,14 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange =(e)=>{
     setNewName(e.target.value)
@@ -14,6 +18,10 @@ const App = () => {
 
   const handleNumberChange =(e)=>{
     setNewNumber(e.target.value)
+  }
+  
+  const handleFilterChange =(e)=>{
+    setFilter(e.target.value)
   }
 
   const handleSubmit=(e)=>{
@@ -40,6 +48,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <>
+        filter shown with <input value={filter}
+            onChange={handleFilterChange}
+          />
+      </>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: 
@@ -57,7 +71,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers numbers={persons} />
+      <Numbers  numbers={persons.filter(
+        person=> person.name.toLowerCase().includes(filter.toLowerCase())
+      )}/>
     </div>
   )
 }

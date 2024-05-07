76ae0b802fdff12bd8e0b2cc736393433edf464b
nicolas240 <nicolasmadero24@gmail.com>
2024-05-07 14:28:26 -0500

    2.13: Phonebook, step 8

--- part2/Phonebook/src/services/persons.js
+++ part2/Phonebook/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
+//----Backend extraido de app.jsx 2.13
 const getAll = () => {
   return axios.get(baseUrl).then(res=>res.data)
 }

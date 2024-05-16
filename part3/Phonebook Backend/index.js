f0eb24df6ea86b9c1877cc3d6a1c7b9f86ca7315
nicolas240 <nicolasmadero24@gmail.com>
2024-05-16 12:02:09 -0500

    3.4: Phonebook Backend, step 4

--- part3/Phonebook Backend/index.js
+++ part3/Phonebook Backend/index.js
@@ -74,11 +74,11 @@ app.get('/info', (request, response) => {
   response.send(message)
 })
 
-/* app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
-}) */
+})
 
 const PORT = 3001
 app.listen(PORT, () => {

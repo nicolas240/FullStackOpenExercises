69ccc853d4dd371992982bf903a757bcb99243e3
nicolas240 <nicolasmadero24@gmail.com>
2024-05-16 11:16:43 -0500

    3.1: Phonebook Backend, step 1

---
+++ part3/Phonebook Backend/index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let personas=[
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+/* 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/personas', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const persona = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+app.get('/api/personas/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const persona = personas.find(persona => persona.id === id)
+    if (persona) {
+        response.json(persona)
+      } else {
+        response.status(404).end()
+      }
+}) */
+
+app.get('/api/personas', (request, response) => {
+  if (personas) {
+      response.json(personas)
+    } else {
+      response.status(404).end()
+    }
+})
+
+/* app.delete('/api/personas/:id', (request, response) => {
+    const id = Number(request.params.id)
+    personas = personas.filter(persona => persona.id !== id)
+    response.status(204).end()
+}) */
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})

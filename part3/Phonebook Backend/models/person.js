54202564ce7d823ed15ebf10b69185ff6bfef319
nicolas240 <nicolasmadero24@gmail.com>
2024-05-28 15:58:27 -0500

    3.22: Lint Configuration, 2

--- part3/Phonebook Backend/models/person.js
+++ part3/Phonebook Backend/models/person.js
@@ -6,8 +6,8 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB',result)
+  .then(()=> {
+    console.log('connected to MongoDB')
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)

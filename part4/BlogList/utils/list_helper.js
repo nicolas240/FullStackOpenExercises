d1b72aacf43355a0723dede4bdc0d09816df918f
nicolas240 <nicolasmadero24@gmail.com>
2024-06-06 15:57:48 -0500

    4.4: Auxiliary functions and unit test, step 2

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,7 +2,25 @@
 const dummy = (blogs) =>{
     return 1
 }
+const totalLikes = (blogs)=>{
+    let total=0
+    blogs.forEach(b => {
+        total=total+b.likes
+    });
+    return total
+}
+
+const authorLike = (blogs,search)=>{
+    let filtered = blogs.filter(b=>b.author.toUpperCase().includes(search.toUpperCase()))
+    return filtered
+}
+
+const titleLike = (blogs,search)=>{
+    let filtered = blogs.filter(b=>b.title.toUpperCase().includes(search.toUpperCase()))
+    console.log('filtered::: ', filtered);
+    return filtered
+}
 
 module.exports = {
-    dummy
+    dummy, totalLikes,authorLike,titleLike
 }
